90987cef9fb6488beacae1fbf381dd2c7046e3d8
OH_MianHu <122138268+OH-MianHu@users.noreply.github.com>
2026-04-23 16:40:06 +0800

    Implement main algorithm in a1_Algorithm.cpp

---
+++ a1_Algorithm.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    long long n,x,ans=0,last=0;
+    cin>>n>>x;
+    for(long long i=0;i<n;i++){
+        long long temp=0;
+        cin>>temp;
+        if(last+temp>x){
+            ans+=last+temp-x;
+            last=x-last;
+        }
+        else{
+            last=temp;
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
